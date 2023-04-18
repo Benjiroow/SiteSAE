90dc668dde8283898d1c657ba2e612a5d6015964
Benjiroow <benjibarthe@gmail.com>
2023-04-18 10:11:14 +0200

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'app/model/dataConnection.php';
-require_once 'app/model/pokedex.model.php';
-
-$databaseConnection = getDatabaseConnection();
-$pokedex = getPokedexWhithoutTypes($databaseConnection);
-$pokedex = addTypesToPokemons($pokedex, $databaseConnection);
