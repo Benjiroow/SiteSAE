bc0a36bc15eec2a2d6783c940c8ffb92a22c46e0
Benjiroow <benjibarthe@gmail.com>
2023-04-18 10:24:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'app/model/dataConnection.php';
 require_once 'app/model/pokedex.model.php';
 
-$databaseConnection = getDatabaseConnection();
+
 $pokedex = getPokedexWhithoutTypes($databaseConnection);
 $pokedex = addTypesToPokemons($pokedex, $databaseConnection);
 
