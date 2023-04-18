8161410a37e3cebcaa83357a0745d1bac6b7d0bf
Benjiroow <benjibarthe@gmail.com>
2023-04-18 08:55:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'app/model/dataConnection.php';
+require_once 'app/model/pokedex.model.php';
+
+$databaseConnection = getDatabaseConnection();
+$pokedex = getPokedexWhithoutTypes($databaseConnection);
+$pokedex = addTypesToPokemons($pokedex, $databaseConnection);
