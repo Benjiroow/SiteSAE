6293cf03e7a1e6fc62836f2b464ea24aca163485
Legendaireyoshi <131138433+Legendaireyoshi@users.noreply.github.com>
2023-06-02 00:14:34 +0200

    accpasfinit
    ,

--- app/view/accueil.view.php
+++ app/view/accueil.view.php
@@ -1,1 +1,20 @@
-Bienvenue ! 
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="public/css/accueil.style.css">
+</head>
+<body>
+
+<h1>Bienvenue</h1>
+
+<div class="pic-ctn">
+    <img src="public/images/anim/1.png" alt="" class="pic">
+    <img src="public/images/anim/2.png" alt="" class="pic">
+    <img src="public/images/anim/3.png" alt="" class="pic">
+    <img src="public/images/anim/4.png" alt="" class="pic">
+    <img src="public/images/anim/5.png" alt="" class="pic">
+    <img src="public/images/anim/6.png" alt="" class="pic">
+  </div>
+
+</body>
+</html>
